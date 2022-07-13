1d27c9407a9ebce6b15f0b887b6a3e72e22c686c
bonideladem <90810699+bonideladem@users.noreply.github.com>
2022-07-13 18:20:27 +0000

    css not working

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import  NavBar from './App';
 import "jquery";
 import "popper.js/dist/umd/popper";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap";
+import  NavBar from './App';
 import "./index.css";
 
 ReactDOM.render(<NavBar/>,document.getElementById("root"));
